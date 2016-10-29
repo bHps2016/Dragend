c0133077c62b3890da3eb2cf2341f00c47d3a5fc
neo1218 <neo1218@yeah.net>
2016-10-30 03:45:31 +0800

    update

--- dragend/app/__init__.py
+++ dragend/app/__init__.py
@@ -1,13 +1,13 @@
 # coding: utf-8
 
+import os
 from flask import Flask
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key is here'
-app.config['QINIU_ACCESS_KEY'] = "7mVlJlbLcnr1m-FbsvOtFlT8aC-6rGBWkdUH0BD6"
-app.config['QINIU_SECRET_KEY'] = "LpuwHp2mECopFk0oPpGbLyjU_tK5ggdwygr0F87r"
-app.config['QINIU_BUCKET_NAME'] = "neo1218"
-app.config['QINIU_BUCKET_DOMAIN'] = "7xj431.com1.z0.glb.clouddn.com"
+app.config['QINIU_ACCESS_KEY'] = os.getenv("DRAG_QINIU_ACCESS_KEY")
+app.config['QINIU_SECRET_KEY'] = os.getenv("DRAG_QINIU_SECRET_KEY")
+app.config['QINIU_BUCKET_NAME'] = os.getenv("DRAG_QINIU_BUCKET_NAME")
+app.config['QINIU_BUCKET_DOMAIN'] = os.getenv("DRAG_QINIU_BUCKET_DOMAIN")
 
 from . import views
